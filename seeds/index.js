497ec935d3cc0e935b476ecba899164f77578267
Jeerasak Kajonrattanavanit <mazuroze@gmail.com>
2024-05-29 22:06:36 +0700

    Adding Basic Styles

--- seeds/index.js
+++ seeds/index.js
@@ -18,14 +18,19 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20);
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/random/?camping",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores soluta quasi nostrum ut nam delectus eum, porro fugit laboriosam! Iste dignissimos assumenda quaerat porro explicabo. Perferendis fuga delectus amet doloribus?",
+      price,
     });
     await camp.save();
   }
 };
 
-seedDB().then(()=>{
-    mongoose.connection.close();
-})
+seedDB().then(() => {
+  mongoose.connection.close();
+});
